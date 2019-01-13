22736cb2692e0b4daa1238dc3e5fb1cf1d27269f
admin <876777919@qq.com>
2019-01-14 01:13:41 +0800

    new push

--- src/Solution24.java
+++ src/Solution24.java
@@ -8,12 +8,19 @@ public class Solution24 {
            if (head==null)
                return null;
            ListNode res=head.next==null?head:head.next;
+           //需要翻转当前元素的条件
            while (cur!=null&&cur.next!=null){
-               pre.next=cur.next;
+               //若temp为空相当于一个终结符
                temp=cur.next.next;
+               //连接之前元素
+               pre.next=cur.next;
+               //相邻元素翻转
                cur.next.next=cur;
+               //连接之后元素
                cur.next=temp;
+               //更新pre指针
                pre=cur;
+               //进入下一组翻转
                cur=cur.next;
            }
         return res;
